56128c74514238e454d5bbd2969a0df4e1562e8b
Omar <70787202+ohc081110@users.noreply.github.com>
2024-02-18 18:18:18 -0600

    Create IPhone14Pro.java

---
+++ IPhone14Pro.java
@@ -0,0 +1,20 @@
+package com.mycompany.factory;
+
+/**
+ *
+ * @author OMAR HC
+ */
+public class IPhone14Pro extends Phone {
+        public IPhone14Pro(){
+
+    }
+    @Override
+    public String getdescription(){
+        return "This is IPhone14 Pro";
+    }
+    @Override
+    public double getPrice(){
+        return 80000.00;
+    }
+    
+}
